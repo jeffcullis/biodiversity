1e9f96b7bc0fb7e323ca070e32dd7f2791e7da51
Jeff Cullis <jeffcullis@gmail.com>
2024-12-04 15:31:47 -0400

    try install2.r for phyloregion install.

--- .devcontainer/install_jupyter_phyloregion.sh
+++ .devcontainer/install_jupyter_phyloregion.sh
@@ -5,3 +5,4 @@ sudo apt upgrade -y
 sudo bash /rocker_scripts/install_jupyter.sh
 sudo bash /rocker_scripts/install_geospatial.sh
 sudo apt install libgmp-dev -y
+install2.r --error --skipinstalled -n 1 rcdd units smoothr phyloregion
